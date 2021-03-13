a56d9c99ec04b984da341584ccd3e4558cb8180c
ctrl98 <390022566@qq.com>
2021-03-13 16:30:23 +0800

    实现单机Eureka服务中心并改造模块成为微服务

---
+++ springcloudHoxton/cloud-eureka-server7001/src/main/java/org/lee/EurekaMain7001.java
@@ -0,0 +1,14 @@
+package org.lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
